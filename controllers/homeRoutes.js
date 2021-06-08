3b6510fc93dce2826a9f02440c8b104bde9ed2e3
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-08 15:57:57 +0930

    Updated homeRoutes get

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,6 @@
 // Contains all user-facing routes
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
 const sequelize = require('./config/connection');
 
 // GET POST renders all posts to homepage
@@ -45,12 +44,23 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET POST renders login page
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+
 // GET POST renders a single post to a page
 router.get('/post/:id', async (req, res) => {
 
     const postData = await Post.findOne({
         where: { 
-            req.params.id
+            id: req.params.id
         },
         attributes: [
             'id',
@@ -96,35 +106,5 @@ router.get('/post/:id', async (req, res) => {
     });
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Post }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// GET POST renders login page
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
-});
 
 module.exports = router;
