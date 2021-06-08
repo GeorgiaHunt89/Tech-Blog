7f1b9004527b919038a77d21f5c1e1bde71d5430
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-08 16:36:05 +0930

    Router to delete user added

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -10,13 +10,15 @@ router.post('/', async (req, res) => {
           email: req.body.email,
           password: req.body.password
       });
+    
+      // excludes password.
+      const { password, ...userDetails } = newUser.get({ plain: true });
   
       req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.username = userData.username;
+        req.session.user = userDetails;
         req.session.logged_in = true;
   
-        res.status(200).json(userData);
+        res.status(200).json(userDetails);
       });
     } catch (err) {
       res.status(400).json(err);
@@ -43,10 +45,11 @@ router.post('/', async (req, res) => {
           .json({ message: 'Incorrect email or password, please try again' });
         return;
       }
+        // excludes password
+      const { password, ...userDetails } = user.get({ plain: true });
   
       req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.username = userData.username;
+        req.session.user = userDetails;
         req.session.logged_in = true;
         
         res.json({ user: userData, message: 'You are now logged in!' });
@@ -58,7 +61,7 @@ router.post('/', async (req, res) => {
   });
 
  // LOG OUT
- router.post('/logout', (req, res) => {
+ router.post('/logout', withAuth, (req, res) => {
     if (req.session.logged_in) {
       req.session.destroy(() => {
         res.status(204).end();
@@ -67,6 +70,27 @@ router.post('/', async (req, res) => {
       res.status(404).end();
     }
   });
-  
+
+// DELETE USER
+router.delete('/:id', withAuth, (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        if (!userData) {
+            res
+              .status(400)
+              .json({ message: 'Incorrect user id, please try again' });
+            return;
+          }
+          res.status(200).json(userDetails);
+      
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
   module.exports = router;
    
