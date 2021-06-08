b6459ade8b8f3250f97d2d92fed42dfa76fcf536
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-08 13:54:22 +0930

    Added sequlize, JAWSDB and localhost requirements

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Imports constructor
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+// JAWSDB_URL to deploy on Heroku
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+   // Local Host requirements
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
