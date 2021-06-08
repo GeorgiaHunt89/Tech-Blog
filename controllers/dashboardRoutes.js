a265adf56341b090b97f51f40ff7e6674d86eebe
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-08 15:55:57 +0930

    Added get router to render edit page

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -4,11 +4,12 @@ const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 const sequelize = require('./config/connection');
 
+// Dashboard displaying posts by logged in user
 // Use withAuth middleware to prevent access to route
 router.get('/', withAuth, async (req, res) => {
     try {
       // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
+      const postData = await Post.findByPk(req.session.user_id, {
         attributes: [
           'id',
           'post_text',
@@ -32,13 +33,47 @@ router.get('/', withAuth, async (req, res) => {
       ],
       });
   
-      const user = userData.get({ plain: true });
+      const post = postData.get({ plain: true });
   
-      res.render('dashboard', {
-        ...user,
-        logged_in: true
-      });
+      res.render('dashboard', { post, logged_in: true });
     } catch (err) {
       res.status(500).json(err);
     }
   });
+
+// Rendered edit page  
+// Use withAuth middleware to prevent access to route
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const postData = await Post.findByPk(req.session.user_id, {
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      {
+        model: User,
+        attributes: ['username']
+      },
+      { exclude: ['password'] }
+    ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('edit-posts', { post, logged_in: true });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
