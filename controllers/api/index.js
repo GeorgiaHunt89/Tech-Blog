a443fd4d716d4eddcef585eb04e22358736e6934
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-09 10:51:17 +0930

    Updated User labels

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const userRoutes = require('./userRoutes.js');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 
-router.use('/users', user-routes);
-router.use('/posts', post-routes);
-router.use('/comments', comment-routes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
