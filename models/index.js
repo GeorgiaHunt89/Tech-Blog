7f7733a4813e5ed78d98c78ca5b06cb20c4645b0
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-09 13:07:58 +0930

    BelongsTo model routes

--- models/index.js
+++ models/index.js
@@ -1,3 +1,37 @@
 const user = require('./user');
 const post = require('./post');
 const comment = require('./comment');
+
+// HasMany
+user.hasMany(post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+user.hasMany(comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+post.hasMany(comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+
+
+// BelongsTo
+post.belongsTo(user, {
+    foreignKey:'user_id'
+});
+
+comment.belongsTo(post, {
+    foreignKey:'post_id'
+});
+
+comment.belongsTo(user, {
+    foreignKey:'user_id'
+});
+
+
+
