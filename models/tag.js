6b8a1e7bba5e5d19ca33c785a1f9470f3a6d3afa
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-09 13:20:16 +0930

    Created tag model and fields

---
+++ models/tag.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class tag extends Model {}
+
+// Creates fields/columns for User model
+tag.init(
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        modelName: 'tag',
+    }
+);
+
+module.exports = tag;
