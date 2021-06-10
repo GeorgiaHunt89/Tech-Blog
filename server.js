c11cef9ca71cd5e64e02bf434814ef76d29b1b2f
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-10 23:21:34 +0930

    Updated Routes

--- server.js
+++ server.js
@@ -1,46 +1,50 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const routes = require('./controllers');
+const path = require("path");
+const sequelize = require("./config/connection.js");
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers");
+const helpers = require("./utils/helper");
 
 // Handlebars.js sets up custom helpers
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
 
-// Helper function
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4234;
 
 // Creates session
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
- 
-app.use(session(sess)); 
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 // Informs template engine for Express.js
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Activates routes
 app.use(routes);
 
 // Actives connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
