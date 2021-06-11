fce2de436f11af270f71929c3349d40f7d4ce286
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-11 17:59:30 +0930

    Changed local host number

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const hbs = exphbs.create({ helpers });
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 4234;
+const PORT = process.env.PORT || 3452;
 
 // Creates session
 const sess = {
@@ -33,7 +33,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Activates routes
 app.use(routes);
