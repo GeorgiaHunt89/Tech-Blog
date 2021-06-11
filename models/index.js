47cebdef583f2e2f73a39b8e3c15f95e8a90be25
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-11 18:06:49 +0930

    Updated handlebars dashboard issue

--- models/index.js
+++ models/index.js
@@ -1,70 +1,51 @@
-const user = require('./user');
-const post = require('./post');
-const comment = require('./comment');
-const tag = require('./tag');
+const user = require("./user");
+const post = require("./post");
+const comment = require("./comment");
 
 // HasMany
 user.hasMany(post, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-  });
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
 user.hasMany(comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 post.hasMany(comment, {
-    foreignKey: 'post_id',
-    onDelete: 'CASCADE'
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
 });
-
-
 
 // BelongsTo
 post.belongsTo(user, {
-    foreignKey:'user_id'
+  foreignKey: "user_id",
 });
 
 comment.belongsTo(post, {
-    foreignKey:'post_id'
+  foreignKey: "post_id",
 });
 
 comment.belongsTo(user, {
-    foreignKey:'user_id'
+  foreignKey: "user_id",
 });
-
-
 
 // BelongsToMany
-post.belongsToMany(tag, {
-    foreignKey: 'post_id',
-    through: 'post_tags',
-    timestamps:false,
-});
-
-tag.belongsToMany(post, {
-    foreignKey: 'tag_id',
-    through: 'tag_tags',
-    timestamps:false,
-});
-
 user.belongsToMany(post, {
-    foreignKey: 'user_id',
-    through: 'user_posts',
-    timestamps:false,
+  foreignKey: "user_id",
+  through: "user_posts",
+  timestamps: false,
 });
 
 user.belongsToMany(comment, {
-    foreignKey: 'user_id',
-    through: 'user_comments',
-    timestamps:false,
+  foreignKey: "user_id",
+  through: "user_comments",
+  timestamps: false,
 });
 
-
 module.exports = {
-    user,
-    post,
-    comment,
-    tag,
+  user,
+  post,
+  comment,
 };
