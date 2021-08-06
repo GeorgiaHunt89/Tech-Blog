bb50bc95857bed798fd2db59faecd60ac9128d06
Georgia Hunt <georgialhunt89@gmail.com>
2021-08-06 11:04:58 +0930

    Made updated changes to find bug

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -66,7 +66,7 @@ router.get("/edit/:id", withAuth, async (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
-    });
+    })
 });
 
 // Renders new post page
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const hbs = exphbs.create({ helpers });
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 2386;
+const PORT = process.env.PORT || 3000;
 
 // Creates session
 const sess = {
